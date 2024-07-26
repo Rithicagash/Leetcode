c3b6a8f029d2f7aa6f207798ea4ecba9eded41ff
Rithicagash John britto <122854305+Rithicagash@users.noreply.github.com>
2024-07-26 12:29:57 +0530

    Update 326. Power of Three.py

--- 326. Power of Three.py
+++ 326. Power of Three.py
@@ -4,8 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n==1 or n==3:
-            return True
-        if n>3 and n%3==0:
-            return self.isPowerOfThree(n/3)
-        return False
+        for i in range(0,31):
+            if n==(3**i):
+                return True
